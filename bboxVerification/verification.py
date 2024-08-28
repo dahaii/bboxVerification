6bc2d61a98a327ca45ad8d2eb64b2d5a4cbf001f
Enes <95295825+dahaii@users.noreply.github.com>
2024-08-28 20:31:27 +0300

    Update verification.py

--- bboxVerification/verification.py
+++ bboxVerification/verification.py
@@ -2,16 +2,41 @@ import cv2
 import os
 from tqdm import tqdm
 
+# Her label için farklı renkler
+label_colors = {
+    0: (0, 255, 0),  # Yeşil
+    1: (255, 0, 0),  # Mavi
+    2: (0, 0, 255)  # Kırmızı
+}
+
+
 # YOLO formatındaki bbox'ları çizme fonksiyonu
 def draw_bboxes(img_path, txt_path, output_path):
     img = cv2.imread(img_path)
+
+    if img is None:
+        print(f"Error: Unable to read image {img_path}. Skipping...")
+        return
+
     height, width, _ = img.shape
+
+    if not os.path.exists(txt_path):
+        print(f"Error: {txt_path} does not exist. Skipping...")
+        return
 
     with open(txt_path, 'r') as file:
         lines = file.readlines()
 
+    if not lines:
+        print(f"Warning: {txt_path} is empty. Skipping...")
+        return
+
     for line in lines:
-        class_id, x_center, y_center, bbox_width, bbox_height = map(float, line.strip().split())
+        try:
+            class_id, x_center, y_center, bbox_width, bbox_height = map(float, line.strip().split())
+        except ValueError:
+            print(f"Error: Invalid bbox format in {txt_path}. Skipping...")
+            return
 
         # YOLO formatından piksel formatına dönüştürme
         x_center = int(x_center * width)
@@ -25,13 +50,17 @@ def draw_bboxes(img_path, txt_path, output_path):
         x2 = int(x_center + bbox_width / 2)
         y2 = int(y_center + bbox_height / 2)
 
+        # Doğru rengi seçmek
+        color = label_colors.get(int(class_id), (255, 255, 255))  # Label yoksa beyaz renk kullan
+
         # Rectangle çizimi
-        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.rectangle(img, (x1, y1), (x2, y2), color, 2)
 
     cv2.imwrite(output_path, img)
 
+
 # Test örneği
-source_folder = "C:/test/train"  # JPG ve TXT dosyalarının bulunduğu klasör
+source_folder = "C:/deneme/test/mix/train"  # JPG ve TXT dosyalarının bulunduğu klasör
 output_folder = os.path.join(source_folder, 'verification/')  # 'verification' klasörünü oluşturma
 
 if not os.path.exists(output_folder):
